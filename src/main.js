31f9f70d64d76c6c6f27ad8428e862096e3f4640
bassem <bassem.saidi03@gmail.com>
2026-04-09 16:53:11 +0100

    Fix CORS links and logo

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
+// Set page title and meta
+document.title = 'BridgeTN - Transport & Logistics Management'
+
+// Create and mount app
 createApp(App).use(router).mount('#app')
